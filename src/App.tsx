4ecf6d941d847dda35fdd2f4adc7a7396c017453
mohamad <mohammadmahdi.daghighi81@gmail.com>
2023-08-17 10:36:43 +0330

    basic functionality of project implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { UnitConverter } from './components/UnitConverter';
+import { Unit } from './types'
 import './App.css';
 
-function App() {
+const LENGTH_UNITS: Unit[] = [
+  { type: 'length', name: 'Meter', conversionFactor: 1},
+  { type: 'length', name: 'Foot', conversionFactor: 3.28084},
+  {type: 'length', name: 'Kilometre', conversionFactor: 0.001},
+  {type: 'length', name: 'Inch', conversionFactor: 39.3701},
+  {type: 'length', name: 'Mile', conversionFactor: 0.000621371}
+];
+
+function App(): JSX.Element {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="App">
+      <UnitConverter initialUnit={LENGTH_UNITS[0]} units={LENGTH_UNITS} />
+    </div>
   );
 }
 
